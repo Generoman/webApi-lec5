a3ba155e871e37439a08d9e4e79560118126c74e
Generoman <53780885+Generoman@users.noreply.github.com>
2022-02-25 11:57:07 +0100

    simple React app

--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ app.post("/login", (req, res) => {
   console.log("POST /login ");
 });
 
+app.use(express.static("public"));
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Running on http://localhost:${server.address().port}`);
 });
